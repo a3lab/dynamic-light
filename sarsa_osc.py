ca235f666cc7a194ae97605b3d26053973552453
Sofian Audry <info@sofianaudry.com>
2018-11-22 14:07:58 +0100

    SARSA OSC algorithm - adapted from keras-rl.

---
+++ sarsa_osc.py
@@ -0,0 +1,44 @@
+import numpy as np
+from gym.spaces import Box, Discrete
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents import SARSAAgent
+from rl.policy import BoltzmannQPolicy
+
+from OscEnv import OscEnv
+
+def reward(observation):
+    return observation[0]
+
+
+# Get the environment and extract the number of actions.
+env = OscEnv(Discrete(3), Box(low=0, high=1, shape=(4,)), reward)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
+
+# Next, we build a very simple model.
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+# SARSA does not require a memory.
+policy = BoltzmannQPolicy()
+sarsa = SARSAAgent(model=model, nb_actions=nb_actions, nb_steps_warmup=10, policy=policy)
+sarsa.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+sarsa.fit(env, nb_steps=100, visualize=False, verbose=2)
+
+# After training is done, we save the final weights.
+sarsa.save_weights('sarsa_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+sarsa.test(env, nb_episodes=5, visualize=True)
